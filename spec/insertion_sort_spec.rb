e741f5d3cc66f13cdf566c36bf7b7f08a9e36c3a
“Matthew <matthew.hinea@gmail.com>
2016-07-15 11:55:26 -0400

    finished insertion_sort and insertion_sort specs, working on bubble sort

---
+++ spec/insertion_sort_spec.rb
@@ -0,0 +1,19 @@
+require 'insertion_sort'
+
+describe "#insertion_sort" do 
+  let(:array){[1, 5, 7, 4, 4, 4, 19, 290, 123098]}
+  let(:neg_array){[1, -5, -2, 1, 12, 4098, -39]}
+
+  it "returns a sorted array" do 
+    expect(insertion_sort(array)).to eq(array.sort)
+  end
+
+  it "returns a sorted array given an array with negative values" do 
+    expect(insertion_sort(neg_array)).to eq(neg_array.sort)
+  end
+
+  it "returns a sorted array" do 
+    expect(insertion_sort(neg_array)).to eq(neg_array.sort)
+  end
+
+end
